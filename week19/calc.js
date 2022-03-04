0df5ef02be28d288b11b5e7ed8350211f74f3c5c
NastyaTarasovaa <tarasowaanastaysa@yandex.ru>
2022-03-04 20:13:06 +0300

    correct calculator

--- week19/calc.js
+++ week19/calc.js
@@ -1,25 +1,40 @@
 
+function calculate (){
 
 let num1 = Number(document.getElementById('num1').value);
 let num2 = Number(document.getElementById('num2').value);
-let numbers = new Calculator(num1,num2);
+let result = new Calculator (num1, num2);
+alert (result);
+}
+
 
 class Calculator {
-  constructor(num1, num2) {
-      this.num1 = num1;
-      this.num2 = num2;
-  }
-
-  static sum(numbers) {
-  return (numbers.num1 + numbers.num2);
-  }
-  static sub(numbers) {
-      return numbers.num1 - numbers.num2;
-  }
-  static mult(numbers) {
-      return numbers.num1 * numbers.num2;
-  }
-  static div(numbers) {
-      return numbers.num1 / numbers.num2;
-  }
+    constructor(num1, num2,) {
+        this.num1 = num1;
+        this.num2 = num2;
+        
+    }
+  
+    static sum() {
+    result = this.num1 + this.num2;
+    }
+    static sub() {
+result = this.num1 - this.num2;
+    }
+    static mult() {
+        result =  this.num1 * this.num2;
+    }
+    static div() {
+        result =  this.num1 / this.num2;
+    }
 }
+
+
+
+
+
+
+
+
+
+
