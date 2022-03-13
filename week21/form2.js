a48442e5de0b60b092eb341e1ef05dc0e6411ffe
NastyaTarasovaa <tarasowaanastaysa@yandex.ru>
2022-03-13 21:57:30 +0300

    add task 3

---
+++ week21/form2.js
@@ -0,0 +1,17 @@
+postButton.onclick = function (e) {
+    e.preventDefault();
+    
+    fetch("https://httpbin.org/post",
+        {
+            method: 'POST',
+            body: new FormData(formElem),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            },
+        })
+        .then(response => response.json())
+        .then(user => {
+            console.log(user);
+        })
+        .catch(error => console.log(error));
+}
