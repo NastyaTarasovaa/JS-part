2be2dc0465e912b59f9e0b81fc103128acc4e51e
NastyaTarasovaa <tarasowaanastaysa@yandex.ru>
2022-03-13 22:41:58 +0300

    last correction

--- week21/search.js
+++ week21/search.js
@@ -1,11 +1,12 @@
 function onSearch () {
     let getGiphy = document.getElementById("giphySearch").value;
 
-    fetch ('https://api.giphy.com/v1/gifs/search?api_key=FapvShXUYLxlkhPH4qmHNsMgzzRSTISF&q=' + getGiphy + '&limit=5&offset=0&rating=g&lang=en')
-        .then(response => response.json())
-        .then(giphy => {
-            document.getElementById("place").src = giphy.url;
-            document.getElementById("qw").innerText = giphy.title;
-        })
-        .catch(error => console.log(error));
+    fetch ('https://api.giphy.com/v1/gifs/search?api_key=FapvShXUYLxlkhPH4qmHNsMgzzRSTISF&q=' + getGiphy+ 'gif&limit=5&offset=0&rating=g&lang=en')
+    .then(response => response.json())
+    .then(giphy => {
+        document.getElementById("address").src = giphy.url;
+        document.getElementById("title").innerText = giphy.title;
+    })
+    .catch(error => console.log(error));
 }
+
