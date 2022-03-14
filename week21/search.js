1befb0097aabfacd20e9f7c76d16ebb2483eb2b3
NastyaTarasovaa <tarasowaanastaysa@yandex.ru>
2022-03-14 21:43:35 +0300

    correct search

--- week21/search.js
+++ week21/search.js
@@ -1,7 +1,7 @@
 function onSearch () {
     let getGiphy = document.getElementById("giphySearch").value;
 
-    fetch ('https://api.giphy.com/v1/gifs/search?api_key=FapvShXUYLxlkhPH4qmHNsMgzzRSTISF&q=' + getGiphy+ 'gif&limit=5&offset=0&rating=g&lang=en')
+    fetch ('https://api.giphy.com/v1/gifs/search?api_key=FapvShXUYLxlkhPH4qmHNsMgzzRSTISF&q=' + getGiphy + 'gif&limit=5&offset=0&rating=g&lang=en')
     .then(response => response.json())
     .then(giphy => {
         document.getElementById("address").src = giphy.url;
