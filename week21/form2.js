cb12fe9eab930f9ddaa979487a88be0043a6fa01
NastyaTarasovaa <tarasowaanastaysa@yandex.ru>
2022-03-14 14:34:20 +0300

    add headers

--- week21/form2.js
+++ week21/form2.js
@@ -5,6 +5,10 @@ postButton.onclick = function (e) {
         {
             method: 'POST',
             body: new FormData(formElem),
+            
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
         })
         .then(response => response.json())
         .then(user => {
